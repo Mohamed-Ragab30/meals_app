877341ff69eed42c5150d8854a8e0bffbbf21469
mohamed-ragab30 <m.ragab@gmx.com>
2021-02-13 12:17:06 +0200

    commet 2

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        accentColor: Colors.lightBlueAccent,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
       ),
       home: CategoriesScreen(),
     );
